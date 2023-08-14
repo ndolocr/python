9117728013be4519b752162172cdbdf1a7ef9aeb
ndolocr <crndolo@gmail.com>
2023-08-14 12:06:07 +0300

    Using Class Based Views.

--- python_lists.py
+++ python_lists.py
@@ -6,12 +6,23 @@ def get_list_length(letter_list):
     list_lenght = len(letter_list)
     return list_lenght
 
+class ListLearning():
+    def __init__(self, l_list):
+        self.letter_list = l_list
+        
+    def display_list(self):        
+        return len(self.letter_list)
+
 def main():
     l = [ 'a', 'b', 'c', 'd']
     
     # Getting lenght of a list
     length_of_list = get_list_length(l)
-    print("Lenght of letter list is -> ", length_of_list)
-    
+    print("Lenght of list from function based view is -> ", length_of_list)
+
+    # Getting length of a list from class based views
+    list_example = ListLearning(l)
+    print("Length of list fom class based view is -> ", list_example.display_list())
+
 if __name__ == '__main__':
     main()

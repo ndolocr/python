c92916e929337540018507fa58aaf3b7a82e7833
ndolocr <crndolo@gmail.com>
2023-08-14 11:53:23 +0300

    Getting the lelngth of a list using a function

--- python_lists.py
+++ python_lists.py
@@ -2,10 +2,16 @@
 This is a file used to lear
 all there is in python LISTS
 """
+def get_list_length(letter_list):
+    list_lenght = len(letter_list)
+    return list_lenght
 
 def main():
     l = [ 'a', 'b', 'c', 'd']
-    print(l)
-
+    
+    # Getting lenght of a list
+    length_of_list = get_list_length(l)
+    print("Lenght of letter list is -> ", length_of_list)
+    
 if __name__ == '__main__':
     main()
